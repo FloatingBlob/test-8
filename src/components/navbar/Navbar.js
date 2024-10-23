e5aa9cf26ae5f19830456b272872846ebd149e1b
Laziness <78thunderstorm@gmail.com>
2024-10-23 22:03:25 +0100

    added info navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Navbar.css";
 import Toggle from "./Toggle.js";
+import { CiCircleInfo } from "react-icons/ci";
 
 export default function NavBar ({ setDarkMode, darkMode }) {
     const icon = "/assets/icon.png";
@@ -11,6 +12,9 @@ export default function NavBar ({ setDarkMode, darkMode }) {
                     <a href="https://hackclub.com/" target="_blank" rel="noopener noreferrer">
                         <img src={icon} className="icon" alt="hack club" />
                     </a>
+                </li>
+                <li >
+                    <a href="#FAQ"> <CiCircleInfo /></a>
                 </li>
                 <li>
                     <Toggle setDarkMode={setDarkMode} darkMode={darkMode} />
